23e66e3d09165941987bbec7447bf63c54a5f1f8
Dries Cruyskens <dries.cruyskens@gmail.com>
2020-06-24 23:05:25 +0200

    Simplified hit sphere math.

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,15 @@ const IMAGE_WIDTH: u32 = 384;
 const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
 
 fn hit_sphere(center: Vec3, radius: f64, r: &Ray) -> f64 {
-    let oc = r.origin - center;
-    let a = r.direction.dot(r.direction);
-    let b = 2.0 * oc.dot(r.direction);
-    let c = oc.dot(oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
+    let oc: Vec3 = r.origin - center;
+    let a: f64 = r.direction.length_squared();
+    let half_b: f64 = oc.dot(r.direction);
+    let c: f64 = oc.length_squared() - radius * radius;
+    let discriminant: f64 = half_b*half_b - a*c;
     if discriminant < 0.0 {
         return -1.0;
     } else {
-        return (-b - discriminant.sqrt()) / (2.0 * a);
+        return (-half_b - discriminant.sqrt()) / a;
     }
 }
 
