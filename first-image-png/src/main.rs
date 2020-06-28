1c9731f29f0e9217348b44539bee556f25e1f082
Dries Cruyskens <dries.cruyskens@gmail.com>
2020-06-28 17:05:56 +0200

    cargo fmt

--- first-image-png/src/main.rs
+++ first-image-png/src/main.rs
@@ -1,6 +1,6 @@
-use std::path::Path;
-use image::{RgbImage, Rgb};
+use image::{Rgb, RgbImage};
 use std::io::{self, Write};
+use std::path::Path;
 
 const IMAGE_WIDTH: u32 = 512;
 const IMAGE_HEIGHT: u32 = 512;
@@ -9,7 +9,7 @@ fn main() -> io::Result<()> {
     let mut img = RgbImage::new(IMAGE_WIDTH, IMAGE_HEIGHT);
 
     // from height -1 up to and including 0
-    for j in (0..=IMAGE_HEIGHT-1).rev() {
+    for j in (0..=IMAGE_HEIGHT - 1).rev() {
         // Writing progress to stdout (using \r to write over same output line).
         io::stdout().write(format!("\rOn scanline: {}", j).as_bytes())?;
         io::stdout().flush()?;
@@ -26,7 +26,7 @@ fn main() -> io::Result<()> {
 
             img.put_pixel(i, j, Rgb([ir, ig, ib]));
         }
-    };
+    }
 
     let path = Path::new("./first-image.png");
     img.save(path).expect("Error saving file");
